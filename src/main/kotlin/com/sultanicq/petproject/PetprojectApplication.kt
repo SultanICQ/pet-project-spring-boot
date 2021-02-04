33f4fcbe8df5f7c93d41c4b25b19c7a46dc0538e
David Garcia Latorre <sultanicq@gmail.com>
2021-02-04 16:04:23 +0100

    Startup

---
+++ src/main/kotlin/com/sultanicq/petproject/PetprojectApplication.kt
@@ -0,0 +1,11 @@
+package com.sultanicq.petproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PetprojectApplication
+
+fun main(args: Array<String>) {
+	runApplication<PetprojectApplication>(*args)
+}
---
+++ src/test/kotlin/com/sultanicq/petproject/PetprojectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sultanicq.petproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PetprojectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
